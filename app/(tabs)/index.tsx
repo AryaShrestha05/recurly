24239bc763ecac1975f7e7b287d6bd6fa8bfcdf6
AryaShrestha05 <aryashrestha005@gmail.com>
2026-07-09 21:07:10 -0400

    feat: tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -20,6 +20,21 @@ export default function App() {
       >
         Sign Up
       </Link>
+      <Link
+        href="/subscriptions/spotify"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Spotify Subscription
+      </Link>
+      <Link
+        href={{
+          pathname: "/subscriptions/[id]",
+          params: { id: "claude" },
+        }}
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Claude Max Subscription
+      </Link>
     </View>
   );
 }
